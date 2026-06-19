bac5792444aecfa9e65affef3bf43e38a459aba2
yuoek <2055048783@qq.com>
2026-06-20 06:58:16 +0800

    starship

--- termux/pkg_install.sh
+++ termux/pkg_install.sh
@@ -2,3 +2,7 @@ pkg install git curl neovim mpv yazi termux-api
 pkg install fluidsynth lilypond
 pkg install lazygit glow gdb chafa
 pip install debugpy
+
+## 2026-06-19
+pkg install kiwix-tools
+kiwix-serve --library ~/kiwix/library.xml --port 8080 --address 0.0.0.0 &
